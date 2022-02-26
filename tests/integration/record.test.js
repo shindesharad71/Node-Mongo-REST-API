50c684620aa9dd8d054de9b117f33d663c33ed3e
Sharad Shinde <sharad.shinde@kuvera.in>
2022-02-26 16:12:38 +0530

    Added: Demo

--- tests/integration/record.test.js
+++ tests/integration/record.test.js
@@ -9,7 +9,16 @@ describe('Record routes', () => {
   describe('GET /records', () => {
     test('should return 400 without query params', async () => {
       const res = await request(app).get('/records').send().expect(httpStatus.BAD_REQUEST);
-      expect(res.body.results).toHaveLength(2);
+    });
+  });
+
+  describe('GET /records?startDate=2016-01-26&endDate=2018-02-02&minCount=2700&maxCount=3000', () => {
+    test('should return 200 with query params', async () => {
+      const res = await request(app)
+        .get('/records?startDate=2016-01-26&endDate=2018-02-02&minCount=2700&maxCount=3000')
+        .send()
+        .expect(httpStatus.OK);
+      expect(res.body.records).toHaveLength(63);
     });
   });
 });
